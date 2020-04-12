eb5c67e7541a7a44c7a80e9a383f47c7eda8a16d
Victor Salvia <vsalviapunsoda@gmail.com>
2020-04-12 16:21:54 +0200

    Cleaned repo

--- .ipynb_checkpoints/GAN-checkpoint.py
+++ .ipynb_checkpoints/GAN-checkpoint.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # # Generative Adversarial Networks - GAN's
 #
 # In this notebook we will generate images of handwritten digits using the MNIST dataset.
@@ -8,7 +9,7 @@
 # 2. [Hyperparameters of the model (easy to modify)](#2nd)
 # 3. [Data preprocessing and EDA](#3rd)
 # 4. [Building the model](#4th)
-# 5. [Hyperparameters of the model (easy to modify)](#5th)
+# 5. [Setting optimization and initialization](#5th)
 # 6. [Hyperparameters of the model (easy to modify)](#6th)
 
 # <div id='1st'/> 
@@ -17,25 +18,26 @@
 
 # Let us first import the necessary libraries:
 
-import os
+# +
+import os #
 import torch #
 import torchvision #
-import torch.nn as nn
+import torch.nn as nn #
 from torchvision import transforms #
-from torchvision.utils import save_image
-from torch.autograd import Variable
-import matplotlib.pyplot as plt
-import pylab
-import numpy as np
+from torchvision.utils import save_image #
+# from torch.autograd import Variable
+import matplotlib.pyplot as plt #
+import numpy as np #
 import sys
-from inception_score import inception_score
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
+# %matplotlib inline
+# -
 
 # Now, the functions auxiliar functions we will need:
 
 # +
-# This
+# This goes from -1, 1 range to 0, 1 range.
 def denorm(x):
     out = (x + 1) / 2
     return out.clamp(0, 1)
@@ -53,15 +55,28 @@ def reset_grad():
 
 # These are the hyperparameters of the model:
 
+# +
 latent_size = 64
 hidden_size = 256
 image_size = 784
-num_epochs = 1 # Initially 300
+num_epochs = 3 # Initially 300
 batch_size = 32
+
 sample_dir = 'samples'
-save_dir = 'save'
+mod_dir = 'save/models'
+vg_dir = 'save/values_graphs'
+
+
+# What are we saving:
+exc_time = 0         # UPDATE every time: 1 for the first time, etc... If I want to try sth 0 :)
+
+# What are we loading:
+ld_time = 0          # exc_time - 1  # Load the last model if not said otherwise
+ld_epoch = 0
+
 interpolate = False
 NN = False
+# -
 
 # <div id='3rd'/>
 #
@@ -111,7 +126,7 @@ print("The pixel intensities range from", int(image.min()), "to", int(image.max(
 
 # Now, let us define the networks we will work with:
 
-# Discriminator
+# +
 D = nn.Sequential(
     nn.Linear(image_size, hidden_size),
     nn.LeakyReLU(0.2),
@@ -120,7 +135,7 @@ D = nn.Sequential(
     nn.Linear(hidden_size, 1),
     nn.Sigmoid())
 
-# Generator
+
 G = nn.Sequential(
     nn.Linear(latent_size, hidden_size),
     nn.ReLU(),
@@ -129,6 +144,13 @@ G = nn.Sequential(
     nn.Linear(hidden_size, image_size),
     nn.Tanh())
 
+# Recall that nn.Sequential(...) are classes.
+# -
+
+# You can see the structure of the networks bellow. The first is the discriminateor and the second is the generator.
+
+# <img src="images/Networks.jpg"  width="50%">
+
 # If we wanted to run this with a GPU we could then do:
 #
 # ``` python
@@ -136,12 +158,18 @@ G = nn.Sequential(
 # G = G.cuda()
 # ```
 
+# <div id='5th'/>
+#
+# ## 5. Setting optimization and initialization
+
 # Let us now set the criteria and the optimizer:
 
 # Binary cross entropy loss and optimizer
-criterion = nn.BCELoss()
+criterion = nn.BCELoss() # criterion(x,y) = 1/N * sum(y_i*log(x_i) + (1-y_i)*log(1-x_i))
 d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0002)
 g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0002)
+
+nn.BCELoss()(torch.ones(10),torch.ones(10))
 
 # Now we initialize statistics that we want to keep track of:
 
@@ -155,118 +183,128 @@ total_step = len(data_loader)
 
 # If we have models saved we can load them:
 
-# Load the model
-state_dict = torch.load('save/G--100.ckpt')
+# +
+# Load the model: the last one is G--[exc_time - 1]--[epoch].ckpt
+
+state_dict = torch.load('G--{}--{}'.format(ld_time,ld_epoch))
 G.load_state_dict(state_dict)
-state_dict = torch.load('save/D--100.ckpt')
+state_dict = torch.load('D--{}--{}'.format(ld_time,ld_epoch))
 D.load_state_dict(state_dict)
+# -
 
 # Let us train our model now :)
 
-# TRAINING!
 for epoch in range(num_epochs):
+    
     for i, (images, _) in enumerate(data_loader):
-        images = images.view(batch_size, -1) #cuda()
-        images = Variable(images)
-        # Create the labels which are later used as input for the BCE loss
+        
+        # Here we resize the images (batch_size x 784)
+        images = images.view(batch_size, -1) #.cuda()
+        
+        # Create the labels which are later used as input for the BCE loss: 0 for fake and 1 for real.
         real_labels = torch.ones(batch_size, 1) #.cuda()
-        real_labels = Variable(real_labels)
         fake_labels = torch.zeros(batch_size, 1) #.cuda()
-        fake_labels = Variable(fake_labels)
 
         # ================================================================== #
         #                      Train the discriminator                       #
         # ================================================================== #
 
-        # Compute BCE_Loss using real images where BCE_Loss(x, y): - y * log(D(x)) - (1-y) * log(1 - D(x))
-        # Second term of the loss is always zero since real_labels == 1
+        # REAL IMAGES LOSS:
+        # d_loss_real = 1/N * sum(log(D(images))) = E_(p_data)[log(D(x))]
         outputs = D(images)
         d_loss_real = criterion(outputs, real_labels)
         real_score = outputs
 
-        # Compute BCELoss using fake images
-        # First term of the loss is always zero since fake_labels == 0
+        # FAKE IMAGES LOSS:
+        # d_loss_fake = 1/N * sum(log(1-D(G(fake_images)))) = E_(p_z)[log(1-D(G(z)))]
         z = torch.randn(batch_size, latent_size) #.cuda()
-        z = Variable(z)
         fake_images = G(z)
         outputs = D(fake_images)
         d_loss_fake = criterion(outputs, fake_labels)
         fake_score = outputs
 
-        # Backprop and optimize
-        # If D is trained so well, then don't update
-        d_loss = d_loss_real + d_loss_fake
-        reset_grad()
-        d_loss.backward()
-        d_optimizer.step()
+        # BACKPROP AND OPTIMIZE:
+        d_loss = d_loss_real + d_loss_fake  # this is a tensor, and has .required_grads = True :)
+        reset_grad()                        # this sets gradients to 0 so that they don't accumulate
+        d_loss.backward()                   # gradients backprop through the graph
+        d_optimizer.step()                  # this changes the values of the parameters following the Adam algorithm.
+        
         # ================================================================== #
         #                        Train the generator                         #
         # ================================================================== #
 
-        # Compute loss with fake images
+        # FAKE IMAGES LOSS:
+        # We train G to maximize log(D(G(z)) instead of minimizing log(1-D(G(z))) [justified in original GANs paper]
         z = torch.randn(batch_size, latent_size) #.cuda()
-        z = Variable(z)
         fake_images = G(z)
         outputs = D(fake_images)
-
-        # We train G to maximize log(D(G(z)) instead of minimizing log(1-D(G(z)))
-        # For the reason, see the last paragraph of section 3. https://arxiv.org/pdf/1406.2661.pdf
         g_loss = criterion(outputs, real_labels)
 
-        # Backprop and optimize
-        # if G is trained so well, then don't update
+        # BACKPROP AND OPTIMIZE:
         reset_grad()
         g_loss.backward()
         g_optimizer.step()
+        
         # =================================================================== #
         #                          Update Statistics                          #
         # =================================================================== #
-        d_losses[epoch] = d_losses[epoch]*(i/(i+1.)) + d_loss.data*(1./(i+1.))
-        g_losses[epoch] = g_losses[epoch]*(i/(i+1.)) + g_loss.data*(1./(i+1.))
-        real_scores[epoch] = real_scores[epoch]*(i/(i+1.)) + real_score.mean().data*(1./(i+1.))
-        fake_scores[epoch] = fake_scores[epoch]*(i/(i+1.)) + fake_score.mean().data*(1./(i+1.))
-
+        
+        # This averages the losses and scores of the epoch across the batches (i).
+        d_losses[epoch] = d_losses[epoch]*(i/(i+1.)) + d_loss*(1./(i+1.)) 
+        g_losses[epoch] = g_losses[epoch]*(i/(i+1.)) + g_loss*(1./(i+1.))
+        
+        # Same as above but these are vectors for each epoch
+        real_scores[epoch] = real_scores[epoch]*(i/(i+1.)) + real_score.mean()*(1./(i+1.)) 
+        fake_scores[epoch] = fake_scores[epoch]*(i/(i+1.)) + fake_score.mean()*(1./(i+1.))
+        
+        # Displays every 200 batches the results of the current batch:
         if (i+1) % 200 == 0:
             print('Epoch [{}/{}], Step [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f}'
-                  .format(epoch, num_epochs, i+1, total_step, d_loss.data, g_loss.data,
-                          real_score.mean().data, fake_score.mean().data))
+                  .format(epoch, num_epochs, i+1, total_step, d_loss, g_loss,
+                          real_score.mean(), fake_score.mean()))
 
-    # Save real images
-    if (epoch+1) == 1:
-        images = images.view(images.size(0), 1, 28, 28)
-        save_image(denorm(images.data), os.path.join(sample_dir, 'real_images.png'))
+    
+    # ================================================================ #
+    #   FOR EACH EPOCH (when finished optimization through batches):   #
+    # ================================================================ #
 
-    # Save sampled images, this are the last ones instanced in the training...
-    fake_images = denorm(fake_images.view(fake_images.size(0), 1, 28, 28)).round() # rounded
-    save_image(fake_images.data, os.path.join(sample_dir, 'fake_images-{}.png'.format(epoch+1)))
+    # Save sampled images (the ones from last batch)
+    fake_images = denorm(fake_images.view(fake_images.size(0), 1, 28, 28)).round() # rounded 0, 1
+    save_image(fake_images, os.path.join(sample_dir, 'fake_images-{}-{}.png'.format(exc_time, epoch+1)))
 
-    # Save and plot Statistics
-    np.save(os.path.join(save_dir, 'd_losses.npy'), d_losses)
-    np.save(os.path.join(save_dir, 'g_losses.npy'), g_losses)
-    np.save(os.path.join(save_dir, 'fake_scores.npy'), fake_scores)
-    np.save(os.path.join(save_dir, 'real_scores.npy'), real_scores)
-
+    # Save Statistics epoch by epoch so that we can have them even if we cancel the execution.
+    np.save(os.path.join(vg_dir, 'd_losses_{}.npy'.format(exc_time)), d_losses)
+    np.save(os.path.join(vg_dir, 'g_losses_{}.npy'.format(exc_time)), g_losses)
+    np.save(os.path.join(vg_dir, 'fake_scores_{}.npy'.format(exc_time)), fake_scores)
+    np.save(os.path.join(vg_dir, 'real_scores_{}.npy'.format(exc_time)), real_scores)
+    
+    # This is a dynamic way of showing the plot. We show the current plots and save it as well.
+    
+    # LOSSES D and G:
     plt.figure()
-    pylab.xlim(0, num_epochs + 1)
+    plt.xlim(0,num_epochs + 1)
     plt.plot(range(1, num_epochs + 1), d_losses, label='d loss')
     plt.plot(range(1, num_epochs + 1), g_losses, label='g loss')
     plt.legend()
-    plt.savefig(os.path.join(save_dir, 'loss.pdf'))
-    plt.close()
-
+    plt.savefig(os.path.join(vg_dir, 'loss_{}.pdf'.format(exc_time)))
+    plt.show()
+    
+    # SCORES real vs fake:
     plt.figure()
-    pylab.xlim(0, num_epochs + 1)
-    pylab.ylim(0, 1)
+    plt.xlim(0, num_epochs + 1)
+    plt.ylim(0, 1)
     plt.plot(range(1, num_epochs + 1), fake_scores, label='fake score')
     plt.plot(range(1, num_epochs + 1), real_scores, label='real score')
     plt.legend()
-    plt.savefig(os.path.join(save_dir, 'accuracy.pdf'))
-    plt.close()
+    plt.savefig(os.path.join(vg_dir, 'accuracy_{}.pdf'.format(exc_time)))
+    plt.show()
 
-    # Save model at checkpoints
+    # Save model at checkpoints: execution time + epoch.
     if (epoch+1) % 10 == 0:
-        torch.save(G.state_dict(), os.path.join(save_dir, 'G--{}.ckpt'.format(epoch+1)))
-        torch.save(D.state_dict(), os.path.join(save_dir, 'D--{}.ckpt'.format(epoch+1)))
+        torch.save(G.state_dict(), os.path.join(mod_dir, 'G--{}--{}.ckpt'.format(exc_time, epoch+1)))
+        torch.save(D.state_dict(), os.path.join(mod_dir, 'D--{}--{}.ckpt'.format(exc_time, epoch+1)))
+
+# * How can I recuperate all the losses and concatenate them together? + Do the final loss and score graph.
 
 # Save the model checkpoints
 torch.save(G.state_dict(), 'G.ckpt')
@@ -319,3 +357,5 @@ if NN == True:
 		print(i)
 
 	save_image(NN.data, os.path.join(sample_dir, 'NN24.png'))
+
+# ## 7. Extras
